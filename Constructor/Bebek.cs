a90ef8d7414bf28fdcc21e3fbd99101e92c7c3af
aysenursezgin <115412288+aysenursezgin@users.noreply.github.com>
2024-08-26 00:01:03 +0300

    Proje dosyası ekle.

---
+++ Constructor/Bebek.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+
+   
+    public class Bebek
+    {
+
+        //Constructor metodu
+        public Bebek()
+        {
+            DogumTarihi = DateTime.Now;
+            Console.WriteLine("Ingaaaaa");
+        }
+        //Bebek ası soyadı dönduren metot Alternatif constructor
+        public Bebek(string ad, string soyad)
+        {
+            Ad = ad;
+            Soyad = soyad;
+            DogumTarihi = DateTime.Now;
+            Console.WriteLine("Ingaaaaa");
+
+        }
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public DateTime DogumTarihi { get; set; }
+    }
+}
---
+++ Constructor/Program.cs
@@ -0,0 +1,25 @@
+﻿using Constructor;
+
+
+
+Bebek bebek1 = new Bebek()
+{
+    Ad = "Alperen",
+    Soyad = "Sezgin",
+    //DogumTarihi = DateTime.Now
+};
+
+
+
+Bebek bebek2 = new Bebek("Emir Asaf", "Algül");
+
+//Property ler konsola yazılır
+Console.WriteLine(bebek1.Ad);
+Console.WriteLine(bebek1.Soyad);
+Console.WriteLine(bebek1.DogumTarihi);
+Console.WriteLine("-----------------");
+
+Console.WriteLine(bebek2.Ad);
+Console.WriteLine(bebek2.Soyad);
+Console.WriteLine(bebek2.DogumTarihi);
+Console.WriteLine("-----------------");
